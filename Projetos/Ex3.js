4bd930e52e1c09f05a31779e126ae25858928c0b
Enzo Monteiro Maciel <enzomontmaciel@gmail.com>
2026-02-18 16:03:11 -0300

    Delete Projetos/Ex3.js

--- Projetos/Ex3.js
+++
@@ -1,9 +0,0 @@
-let nome = "Enzo";
-let idade = 19;
-let estudante = true;
-let capitalSocial = undefined;
-
-console.log(typeof nome)
-console.log(typeof idade)
-console.log(typeof estudante)
-console.log(typeof capitalSocial)
