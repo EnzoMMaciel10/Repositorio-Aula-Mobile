8f533d6d31b026fbd1a35a20874f4822fbb5991e
Enzo Monteiro Maciel <enzomontmaciel@gmail.com>
2026-02-18 16:03:27 -0300

    Delete Projetos/Ex5.js

--- Projetos/Ex5.js
+++
@@ -1,7 +0,0 @@
-let existem = { nome : "Enzo", matricula : "563734"};
-console.log(existem.nome)
-console.log(existem.matricula)
-console.log(existem.nota)
-existem.nota = 10
-console.log(existem.nota)
-
